b90a527fc47d035567633ac27d22198bedd787e3
henryrgithub <hroeramsey@gmail.com>
2024-07-12 12:21:36 -0700

    basic airfoil physics calcs implemented

--- src/plane.ts
+++ src/plane.ts
@@ -1,8 +1,4 @@
 // todo:
-// - Create pitch control derivatives
-// -- This will allow plane-specific physics calculations
-// -- Implement these derivatives as a tensor to allow sensible matrix mult
-// -- Use mathjs for matrix mult implementation
 // - Create geometry more dynamically
 // -- Import structure that represents shape of plane, conver to group of extrudeGeometries
 // -- Have default geometry
@@ -10,13 +6,11 @@
 
 import * as THREE from "three";
 import {
-  ExtendedJSONSchema,
-  FromExtendedSchema,
   FromSchema,
-  JSONSchema,
 } from "json-schema-to-ts";
 //import {matrixMult3x3} from './helpers.ts';
 import { createAirfoilGeometry } from "./airfoilGeometry.js";
+import { log } from "console";
 
 enum InterruptTypes {
   YAW = "YAW",
@@ -78,8 +72,6 @@ export const planeSchema = {
     bodies: {
       type: "array",
       items: {
-        //$ref: 'bodySchema.json',
-        //$ref: 'http://example.com/schemas/bodySchema.json',
         type: "object",
         properties: {
           name: { type: "string" },
@@ -103,13 +95,11 @@ export const planeSchema = {
           "coeffFricUL",
         ],
         additionalProperties: false,
-        //type: 'number',
       },
     },
     surfaces: {
       type: "array",
       items: {
-        //$ref: 'surfaceSchema.json',
         type: "object",
         properties: {
           name: { type: "string" },
@@ -124,7 +114,6 @@ export const planeSchema = {
           foilSections: {
             type: "array",
             items: {
-              //$ref: 'airfoilSchema.json',
               type: "object",
               properties: {
                 name: { type: "string" },
@@ -172,7 +161,6 @@ export const planeSchema = {
                 "cMCurve",
               ],
               additionalProperties: false,
-              //type: 'airfoilSchema',
             },
             minItems: 1,
           },
@@ -221,12 +209,8 @@ export const planeSchema = {
 } as const;
 export type PlaneSpecs = FromSchema<typeof planeSchema>;
 export type BodySpecs = FromSchema<typeof planeSchema.properties.bodies.items>;
-export type SurfaceSpecs = FromSchema<
-  typeof planeSchema.properties.surfaces.items
->;
-export type FoilSpecs = FromSchema<
-  typeof planeSchema.properties.surfaces.items.properties.foilSections.items
->;
+export type SurfaceSpecs = FromSchema<typeof planeSchema.properties.surfaces.items>;
+export type FoilSpecs = FromSchema<typeof planeSchema.properties.surfaces.items.properties.foilSections.items>;
 
 class AeroBody {
   model: THREE.Mesh;
@@ -235,7 +219,6 @@ class AeroBody {
   private heightm: number;
   private posm: number[];
   private coeffFricUL: number;
-  //constructor(specsIn: BodySpecs) {
   constructor(specsIn: BodySpecs) {
     this.lengthm = specsIn.lengthm;
     this.widthm = specsIn.widthm;
@@ -247,8 +230,10 @@ class AeroBody {
       this.widthm,
       this.heightm,
     );
-    const material = new THREE.MeshNormalMaterial();
+    //const material = new THREE.MeshNormalMaterial();
+    const material = new THREE.MeshPhongMaterial({ color: '#8AC' });
     const boxMesh = new THREE.Mesh(boxGeo, material);
+    boxMesh.castShadow = true;
     boxMesh.translateX(-this.lengthm / 2);
     this.model = boxMesh;
   }
@@ -257,6 +242,8 @@ class AeroBody {
 class Airfoil {
   private static readonly SWEEP_LE_RAD_STANDIN = 0;
   private static readonly SWEEP_TE_RAD_STANDIN = 0;
+  private static readonly COEFF_DRAG_FLAT_PARALLEL = 0.005;
+  private static readonly COEFF_DRAG_FLAT_PERPENDIC = 1.2;
 
   public model: THREE.Mesh;
   private name: string;
@@ -270,7 +257,8 @@ class Airfoil {
   private sweepTErad: number;
   private tipChordm: number;
   private aeroAream2: number;
-  private forcePos: number[];
+  private posAeroCenter: number[];
+  private centroid: number[];
   private cLCurve: number[][];
   private cDCurve: number[][];
   private cMCurve: number[][];
@@ -298,14 +286,16 @@ class Airfoil {
     const taperRatio = this.tipChordm / this.rootChordm;
     const aeroCenterPosY = ((this.lengthm / 3) * (1 + 2 * taperRatio)) /
       (1 + taperRatio);
-    const aeroCenterChord = aeroCenterPosY *
+    const aeroCenterChord = this.rootChordm + aeroCenterPosY *
       (Math.tan(this.sweepTErad) - Math.tan(this.sweepLErad));
     const aeroCenterPosX = -Math.tan(this.sweepLErad) * aeroCenterPosY -
       0.25 * aeroCenterChord;
-    this.forcePos = [aeroCenterPosX, aeroCenterPosY, 0.0];
+    this.posAeroCenter = [aeroCenterPosX, aeroCenterPosY, 0.0];
+    const centroidX = -Math.tan(this.sweepLErad) * aeroCenterPosY -
+      0.5 * aeroCenterChord;
+    this.centroid = [centroidX, aeroCenterPosY, 0.0];
 
-    this.aeroAream2 = this.lengthm * this.rootChordm;
-
+    this.aeroAream2 = (this.rootChordm + this.tipChordm) * this.lengthm / 2;
     const airfoilGeo = createAirfoilGeometry(
       this.lengthm,
       this.sweepLErad,
@@ -314,37 +304,92 @@ class Airfoil {
       this.thicknessUL * this.rootChordm,
       this.thicknessUL * this.tipChordm,
     );
-
-    const boxGeo = new THREE.BoxGeometry(
-      this.rootChordm,
-      this.lengthm,
-      this.rootChordm * this.thicknessUL,
-    );
-    const material = new THREE.MeshNormalMaterial();
+    //const material = new THREE.MeshNormalMaterial();
     const color = new THREE.Color(Math.floor(Math.random() * (256 ** 3)));
     //const simpleMaterial = new THREE.MeshBasicMaterial({color:color});
-    const simpleMaterial = new THREE.MeshBasicMaterial({
-      color: color,
-      side: THREE.DoubleSide,
-    });
-    const boxMesh = new THREE.Mesh(boxGeo, material);
-    //boxMesh.translateY(-this.lengthm);// 2);
+    const simpleMaterial = new THREE.MeshPhongMaterial({ color: '#8AC' });
+    //const simpleMaterial = new THREE.MeshBasicMaterial({
+    //  color: color,
+    //  side: THREE.DoubleSide,
+    //});
     const airfoilMesh = new THREE.Mesh(airfoilGeo, simpleMaterial);
+    airfoilMesh.castShadow = true;
     this.model = airfoilMesh;
-    //this.model = boxMesh;
   }
 
   getTipCoords(): number[] {
     return this.tipCoords;
   }
+  findForceandMoment(relWindmps: THREE.Vector3,airDenskgPerm3: number, angleOfAttackdeg: number,combinedFlow: number): ForceMovementStruct {
+    let aoADefined = this.checkAoA(angleOfAttackdeg);
+    if(aoADefined){
+      let dynPressPa = airDenskgPerm3 * (combinedFlow ** 2) / 2
+      let cL = this.interpolateCoeff(this.cLCurve,angleOfAttackdeg);
+      let lift =  cL * dynPressPa * this.aeroAream2;
+      let cD = this.interpolateCoeff(this.cDCurve,angleOfAttackdeg);
+      let drag =  cD * dynPressPa * this.aeroAream2;
+      let cM = this.interpolateCoeff(this.cMCurve,angleOfAttackdeg);
+      let moment = cM * dynPressPa * this.aeroAream2 * (this.rootChordm + this.tipChordm)/2;
+      let mainAeroForce = new THREE.Vector3(-drag,0,lift);
+      let mainAeroMoment = new THREE.Vector3(0,-moment,0);
+
+      let cross = Airfoil.COEFF_DRAG_FLAT_PARALLEL * airDenskgPerm3 * (relWindmps.getComponent(1) ** 2) * this.aeroAream2;
+      let crossAeroForce = new THREE.Vector3(0,cross,0);
+      let crossAeroMoment = new THREE.Vector3(0,0,0);
+            
+      let mainAeroForceMomentUntranslated = new ForceMovementStruct(mainAeroForce,mainAeroMoment);
+      let crossAeroForceMomentUntranslated = new ForceMovementStruct(crossAeroForce,crossAeroMoment);
+
+      let mainAeroForceMomentTranslated = mainAeroForceMomentUntranslated.translate(this.posAeroCenter);
+      let crossAeroForceMomentTranslated = crossAeroForceMomentUntranslated.translate(this.centroid);
+
+      let combinedForceMoment = mainAeroForceMomentTranslated.sumWith(crossAeroForceMomentTranslated);
+      let zeroCoordForceMoment = combinedForceMoment.translate(this.rootCoords);
+      return zeroCoordForceMoment;
+      //return combinedForceMoment;
+    }
+    else{
+      let cross = Airfoil.COEFF_DRAG_FLAT_PARALLEL * airDenskgPerm3 * (relWindmps.getComponent(1) ** 2) * this.aeroAream2;
+      let drag = Airfoil.COEFF_DRAG_FLAT_PARALLEL * airDenskgPerm3 * (relWindmps.getComponent(0) ** 2) * this.aeroAream2;
+      let lift = Airfoil.COEFF_DRAG_FLAT_PERPENDIC * airDenskgPerm3 * (relWindmps.getComponent(2) ** 2) * this.aeroAream2;
+
+      let aeroForce = new THREE.Vector3(drag,cross,lift);
+      let aeroMoment = new THREE.Vector3(0,0,0);
+      let aeroForceMomentUntranslated = new ForceMovementStruct(aeroForce,aeroMoment);
+      let aeroForceMomentTranslated = aeroForceMomentUntranslated.translate(this.centroid);
+      let zeroCoordForceMoment = aeroForceMomentTranslated.translate(this.rootCoords);
+      return zeroCoordForceMoment;
+    }
+
+  }
+  interpolateCoeff(coeffArray: number[][], angleOfAttackdeg: number): number {
+    let aoAs = coeffArray.map((x) => x[0]);
+    let i = aoAs.findIndex((x) => x > angleOfAttackdeg) - 1;
+    let xStep = aoAs[i+1] - aoAs[i];
+    let xDiff = angleOfAttackdeg - aoAs[i];
+    let cZero = coeffArray[i][1];
+    let cOne = coeffArray[i+1][1];
+    let yStep = cOne - cZero;
+    return cZero + yStep * (xDiff / xStep);
+  }
+
+  checkAoA(aoAdeg: number): boolean {
+    //console.log("max AoA " + this.name + ": " + this.cLCurve[this.cLCurve.length-1][0]); 
+    //console.log("min AoA " + this.name + ": " + this.cLCurve[0][0]);
+    if ((aoAdeg < this.cLCurve[this.cLCurve.length-1][0]) && 
+        (aoAdeg > this.cLCurve[0][0])){
+      return true;
+    }
+    return false;
+  }
 
   //calcForces
 }
 
-//class AeroForces
-
 class AeroSurface {
   public model: THREE.Group;
+  public name: string;
+  public rotationAdj: THREE.Matrix4;
   private isMirror: boolean;
   private rootPosm: number[];
   private rotationrad: number;
@@ -353,6 +398,8 @@ class AeroSurface {
     this.isMirror = mirror;
     this.rootPosm = specsIn.rootPosm;
     this.rotationrad = specsIn.rotationrad;
+    this.name = specsIn.name;
+
 
     this.model = new THREE.Group();
     this.model.translateX(this.rootPosm[0]);
@@ -360,32 +407,74 @@ class AeroSurface {
     this.model.translateZ(this.rootPosm[2]);
     this.model.rotateX(this.rotationrad);
     this.foilSections = [];
+    this.rotationAdj = new THREE.Matrix4().identity(); //identity technically not needed as cunstructor initializes to identity
     for (const foilSpec of specsIn.foilSections) {
       const foil = new Airfoil(foilSpec, [0, 0, 0]);
       this.foilSections.push(foil);
       this.model.add(foil.model);
     }
     if (mirror) {
+      this.rotationAdj.set(-1,0,0,0,
+                          0,-1,0,0,
+                          0,0,1,0,
+                          0,0,0,1);
       this.model.scale.multiply(new THREE.Vector3(1, -1, 1));
       this.model.translateY(this.rootPosm[1] * (-2));
     }
+      
+  }
+  findForceandMoment(relWindmps: THREE.Vector3, airDenskgPerm3: number): ForceMovementStruct{
+    
+      let angleOfAttackdeg = Math.atan(relWindmps.getComponent(2)/relWindmps.getComponent(0))*180/Math.PI;
+      let combinedFlow = Math.sqrt((relWindmps.getComponent(2)**2) + (relWindmps.getComponent(0)**2));
+      for (const foil of this.foilSections) {
+        let foilInfluences = foil.findForceandMoment(relWindmps,airDenskgPerm3,angleOfAttackdeg,combinedFlow);
+      }
+      /* FIXME */
+      return new ForceMovementStruct(new THREE.Vector3(),new THREE.Vector3());
+
+
+  }
+  //isAoADefineddeg(aoaDeg: number){
+}
+class ForceMovementStruct{
+  public forceN: THREE.Vector3;
+  public momentNm: THREE.Vector3;
+  constructor(forceN: THREE.Vector3, momentNm: THREE.Vector3){
+    this.forceN = forceN;
+    this.momentNm = momentNm;
+  }
+  translate(translateFrom: number[]): ForceMovementStruct{
+    let forceN = this.forceN.clone();
+    let momentNm = this.momentNm.clone();
+    let vectorDist = new THREE.Vector3(translateFrom[0],translateFrom[1], translateFrom[2]);
+    let translationMomentNm = vectorDist.clone().cross(forceN);
+    let totMoment = momentNm.clone().add(translationMomentNm);
+    return new ForceMovementStruct(forceN,totMoment);
+  }
+  sumWith(toSum: ForceMovementStruct):ForceMovementStruct{
+    let forceN = this.forceN.clone().add(toSum.forceN.clone());
+    let momentNm = this.momentNm.clone().add(toSum.momentNm.clone());
+    return new ForceMovementStruct(forceN,momentNm);
   }
 }
 
 class FlightState {
-  public velocitymPers: number[];
-  public rotRatesradPers: number[];
+  public velocitymPers: THREE.Vector3;
+  public rotRatesradPers: THREE.Vector3;
 
-  constructor(startingVel = [10, 0, 0]) {
-    this.velocitymPers = startingVel;
-    this.rotRatesradPers = [0, 0, 0];
+  constructor(startingVel: THREE.Vector3 ) {
+    this.velocitymPers = new THREE.Vector3;
+    this.velocitymPers.setX(10.0);
+    this.velocitymPers.setY(1.0);
+    this.rotRatesradPers = new THREE.Vector3;
   }
 }
 export class Plane {
-  private static readonly MATERIAL = new THREE.MeshNormalMaterial();
   private static readonly FUSEHEADONCF = 0.1;
   private static readonly FUSESIDEONCF = 1.0;
   private static readonly FUSESIDONANGRAD = 0.35;
+  private static readonly ASLAIRDENSKGPERM3 = 1.225;
   private aeroBodies: AeroBody[];
   private aeroSurfaces: AeroSurface[];
   model: THREE.Group;
@@ -421,14 +510,33 @@ export class Plane {
     //this.calcAccelerations(time);
     //this.calcDisplacements(time);
   }
-  calcAeroForces(): THREE.Vector3 {
+  calcAeroForces(windDirec: THREE.Quaternion, windVelmps: number): THREE.Vector3 {
     let planeQuat = new THREE.Quaternion();
     this.model.getWorldQuaternion(planeQuat);
-    console.log(planeQuat);
+    let planeQuatInv = planeQuat.clone();
+    planeQuatInv.conjugate();
+    let forceN = new THREE.Vector3();
+    let momentsNm = new THREE.Vector3();
+    //console.log(planeQuat);
     for (const surf of this.aeroSurfaces) {
       let test = new THREE.Quaternion();
       surf.model.getWorldQuaternion(test);
-      console.log(test);
+      let conj = test.clone();
+      conj.conjugate();
+      let relrot = new THREE.Quaternion();
+      relrot.multiplyQuaternions(test,planeQuatInv);
+      let relRotMatrix = new THREE.Matrix4;
+      relRotMatrix.makeRotationFromQuaternion(relrot);
+      relRotMatrix.multiply(surf.rotationAdj);
+      let transformedVelmps = this.flightState.velocitymPers.clone();
+      transformedVelmps.applyMatrix4(relRotMatrix);
+      let angleOfAttackdeg = Math.atan(transformedVelmps.getComponent(2)/transformedVelmps.getComponent(0))*180/Math.PI;
+      surf.findForceandMoment(transformedVelmps,Plane.ASLAIRDENSKGPERM3)
+
+
+
+
+      //console.log("surface: ", surf.name, ", rel rot: ", relrot, ", transformed vel: ", transformedVelmps, ", AoA: " + angleOfAttackdeg);
     }
     for (const body of this.aeroBodies) {
       let test = new THREE.Quaternion();
--- src/vis.ts
+++ src/vis.ts
@@ -5,8 +5,10 @@ import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 export class Vis {
   private renderer: THREE.WebGLRenderer;
   private scene: THREE.Scene;
+  private light: THREE.DirectionalLight;
   private camera: THREE.PerspectiveCamera;
   private aircraftModels: THREE.Group;
+  private ground: THREE.Group;
   private camControls: OrbitControls;
   private static readonly camFov = 70;
   private static readonly camAspectDefault = 1; //should be overwritten on attach or resize
@@ -21,20 +23,65 @@ export class Vis {
       Vis.camNearFrustum,
       Vis.camFarFrustum
     );
-    this.camera.position.z = 1;
+    this.camera.position.set( 0, -1, 1.5 );
 
     this.scene = new THREE.Scene();
+
+    const color = 0xFFFFFF;
+		const intensity = 3;
+		this.light = new THREE.DirectionalLight( color, intensity );
+    this.light.castShadow = true;
+    this.light.shadow.mapSize.width = 512; // default
+    this.light.shadow.mapSize.height = 512;
+		this.light.position.set( 5, 0, 10 );
+    //this.light.rotateX(1.7);
+		this.light.target.position.set( 0 , 0, 0 );
+		this.scene.add( this.light );
+		this.scene.add( this.light.target );
 
     this.aircraftModels = new THREE.Group();
     this.aircraftModels.name = 'aircraftModels';
     this.scene.add(this.aircraftModels);
 
+    this.ground = this.makeGround();
+    this.ground.name = 'ground';
+    this.scene.add(this.ground);
+
     this.renderer = new THREE.WebGLRenderer({antialias: true});
+    this.renderer.shadowMap.enabled = true;
+    //this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+    this.renderer.shadowMap.type = THREE.PCFShadowMap;
     this.camControls = new OrbitControls(this.camera, this.renderer.domElement);
     this.camControls.update();
 
     this.animationCallbacks = [];
     this.addAnimationCallback(this.drawFrame);
+  }
+
+  makeGround(): THREE.Group {
+    const planeSize = 20;
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load( 'https://cdn.polyhaven.com/asset_img/primary/brown_mud_leaves_01.png' );
+    //const texture = loader.load( 'https://threejs.org/manual/examples/resources/images/checker.png' );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    //texture.colorSpace = THREE.SRGBColorSpace;
+    const repeats = planeSize / 2;
+    texture.repeat.set( repeats, repeats );
+    const planeGeo = new THREE.PlaneGeometry( planeSize, planeSize );
+    const planeMat = new THREE.MeshPhongMaterial( {
+      //color: 0x49eb34
+      map: texture,
+      side: THREE.DoubleSide,
+    } );
+    const mesh = new THREE.Mesh( planeGeo, planeMat );
+    mesh.receiveShadow = true;
+
+    const groundGroup = new THREE.Group();
+    groundGroup.add(mesh);
+    return groundGroup;
+
   }
 
   addAnimationCallback(funcIn: Function) {
